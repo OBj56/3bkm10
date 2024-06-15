bf89d878979148fb2d7d5403546c7a0f620461f6
musa islam <hamerofth@gmail.com>
2024-06-15 18:53:45 -0400

    "Breyk"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿ // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+class Perfect
+{
+    static void Main()
+    {
+       Write("");
+        int number = int.Parse(ReadLine());
+
+        if (Perfection(number))
+        {
+            Write("Perfect Number");
+        }
+        else
+        {
+            Write("Not A Perfect Number");
+        }
+    }
+
+    static bool Perfection(int number)
+    {
+        int sum = 0;
+        for (int a = 1; a < number; a++)
+        {
+            if (number % a == 0)
+            {
+                sum += a;
+            }
+        }
+        return sum == number;
+    }
+}
