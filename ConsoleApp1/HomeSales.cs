b38a24fd19784292cf129175f26759e68ddbd4cd
musa islam <hamerofth@gmail.com>
2024-06-16 13:28:25 -0400

    123

--- ConsoleApp1/HomeSales.cs
+++ ConsoleApp1/HomeSales.cs
@@ -1,62 +1,66 @@
 ﻿using System;
 using static System.Console;
-public class HomeSales
+
+namespace ConsoleApp1
 {
-    public static void Main()
+    public class HomeSales
     {
-
-        int daniel = 0;
-        int edward = 0;
-        int francis = 0;
-
-        
-        char topearner = '\0';
-
-
-        int mostmoney = 0;
-        WriteLine("Enter the salesperson initials (D, E, or F) or Z to quit:");
-
-
-        while (true)
+        public static void Main()
         {
-            Write("salesperson: ");
-            char names = char.ToUpper(ReadKey().KeyChar);
+
+            int daniel = 0;
+            int edward = 0;
+            int francis = 0;
+
+
+            char topearner = '\0';
+
+
+            int mostmoney = 0;
+            WriteLine("Enter the salesperson initials (D, E, or F) or Z to quit:");
+
+
+            while (true)
+            {
+                Write("salesperson: ");
+                char names = char.ToUpper(ReadKey().KeyChar);
+                WriteLine();
+
+                if (names == 'Z')
+                    break;
+
+                if (names != 'D' && names != 'E' && names != 'F')
+                {
+                    WriteLine("intermediate output: Error, invalid salesperson selected, please try again");
+                    continue;
+                }
+
+                Write("sale: ");
+                int sale = int.Parse(ReadLine());
+
+
+                if (names == 'D')
+                    daniel += sale;
+                else if (names == 'E')
+                    edward += sale;
+                else if (names == 'F')
+                    francis += sale;
+
+                if (sale > mostmoney)
+                {
+                    mostmoney = sale;
+                    topearner = names;
+                }
+            }
+
+
+            int grandTotal = daniel + edward + francis;
+
             WriteLine();
+            WriteLine($"Grand Total: {grandTotal}");
+            WriteLine($"Highest Sale: {topearner}");
 
-            if (names == 'Z')
-                break;
-
-            if (names != 'D' && names != 'E' && names != 'F')
-            {
-                WriteLine("intermediate output: Error, invalid salesperson selected, please try again");
-                continue;
-            }
-
-            Write("sale: ");
-            int sale = int.Parse(ReadLine());
-
-    
-            if (names == 'D')
-              daniel += sale;
-            else if (names == 'E')
-               edward += sale;
-            else if (names == 'F')
-                francis += sale;
-
-            if (sale > mostmoney)
-            {
-                mostmoney = sale;
-                topearner = names;
-            }
+            ReadKey();
         }
-
-   
-        int grandTotal = daniel + edward + francis;
-
-        WriteLine();
-        WriteLine($"Grand Total: {grandTotal}");
-        WriteLine($"Highest Sale: {topearner}");
-
-        ReadKey();
     }
 }
